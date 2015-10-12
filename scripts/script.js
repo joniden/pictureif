1d34b0fe78ec4af6ce9102deebaa8bc062c46038
Joacim Nidén <jocke@Joacims-MBP.dhcp.sthlm.hyperisland.local>
2015-10-12 16:54:17 +0200

    Fixed swiper
    Swiper now only swipes horizontal on pictures
    Fixed next and prev buttons
    Made button icon more sharp.

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,35 @@
 $(document).ready(function () {
-    //initialize swiper when document ready  
-    var swiperH = new Swiper ('.swiper-container-h', {
-      	// Optional parameters
-	    direction: 'horizontal',
-	    loop: false,
-	    nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        hashnav: true,
-        //autoplay: 2500,
-        autoplayDisableOnInteraction: true
-    })   
+  //initialize swiper when document ready  
+  var swiperH = new Swiper ('.swiper-container-h', {
+    	// Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    keyboardControl: true,
+
+    hashnav: true,
+    //autoplay: 2500,
+    autoplayDisableOnInteraction: true
+
+  });   
 
  	var swiperV = new Swiper ('.swiper-container-v', {
-      	// Optional parameters
-      	direction: 'vertical',
-      	loop: false,
-      	onClick: function(i,e){
-      		i.slideNext();
-      		
-      	}
-
-    })
-
-  });
+  	// Optional parameters
+  	direction: 'vertical',
+  	loop: false,
+  	onClick: function(i,e){
+  		//i.slideNext();
+  		
+  	},
+    onSlideChangeEnd: function(i){
+      if(i.isBeginning == false){
+        
+        swiperH.lockSwipes();
+      }else{
+        swiperH.unlockSwipes();
+      }
+      
+    }
+  })
+});
